a1551848543e08594397245d34d6110ee7ba0efe
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-28 16:18:00 +0300

    Update AsasecImgui.mm

--- MULTI-TOOL/Imgui/Gui/AsasecImgui.mm
+++ MULTI-TOOL/Imgui/Gui/AsasecImgui.mm
@@ -1,13 +1,39 @@
 #import "AsasecImgui.h"
 
 #import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+
+#include "../imgui.h"
+#include "../Backends/imgui_impl_metal.h"
+
+static bool g_ImGuiInitialized = false;
 
 void ASASECImGuiStart(void)
 {
-    NSLog(@"[ASASEC] ImGui system started");
+    if (g_ImGuiInitialized)
+        return;
+
+    g_ImGuiInitialized = true;
+
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    io.IniFilename = nullptr;
+
+    ImGui::StyleColorsDark();
+
+    NSLog(@"[ASASEC] Dear ImGui initialized");
 }
 
 void ASASECImGuiStop(void)
 {
-    NSLog(@"[ASASEC] ImGui system stopped");
+    if (!g_ImGuiInitialized)
+        return;
+
+    g_ImGuiInitialized = false;
+
+    ImGui::DestroyContext();
+
+    NSLog(@"[ASASEC] Dear ImGui stopped");
 }
