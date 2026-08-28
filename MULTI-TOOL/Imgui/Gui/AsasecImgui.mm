ed57437e6ec1d7b04f3bb1773093e2eee6510980
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-28 15:25:14 +0300

    Create AsasecImgui.mm

---
+++ MULTI-TOOL/Imgui/Gui/AsasecImgui.mm
@@ -0,0 +1,13 @@
+#import "AsasecImgui.h"
+
+#import <UIKit/UIKit.h>
+
+void ASASECImGuiStart(void)
+{
+    NSLog(@"[ASASEC] ImGui system started");
+}
+
+void ASASECImGuiStop(void)
+{
+    NSLog(@"[ASASEC] ImGui system stopped");
+}
