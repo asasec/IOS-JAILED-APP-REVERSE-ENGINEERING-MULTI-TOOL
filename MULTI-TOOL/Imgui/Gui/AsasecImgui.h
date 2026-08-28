920c0c933c39bab42bb0e2115f5d0ec75f7af6a7
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-28 15:33:40 +0300

    Update AsasecImgui.h

--- MULTI-TOOL/Imgui/Gui/AsasecImgui.h
+++ MULTI-TOOL/Imgui/Gui/AsasecImgui.h
@@ -1,7 +1,15 @@
 #ifndef ASASECIMGUI_H
 #define ASASECIMGUI_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void ASASECImGuiStart(void);
 void ASASECImGuiStop(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
