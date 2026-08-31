5e5f8f6c7c722f0b0cf951e9ea9583d72f16f43f
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-31 14:59:32 +0300

    Update and rename GA.txt to GuiAlert.h

---
+++ MULTI-TOOL/GuiAlert/GuiAlert.h
@@ -0,0 +1,31 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GuiAlert : NSObject
+
++ (void)BilgiAktarBaslik:(NSString *)baslik mesaj:(NSString *)mesaj;
+
++ (void)BaslangicEkraniWithFlexGui:(void (^)(void))flexGui
+                         dinleyici:(void (^)(void))dinleyici
+                      storeKitHook:(void (^)(void))storeKitHook
+                             kapat:(void (^)(void))kapat;
+
++ (void)DinleyiciMenuBaslik:(NSString *)baslik;
+
++ (void)OyunaOzelModlu:(void (^)(void))modlu
+                modsuz:(void (^)(void))modsuz
+                mesaj:(NSString *)mesaj;
+
++ (void)SonUyariYapi:(NSString *)yapi
+            devamEt:(void (^)(void))devamEt
+           iptalEt:(void (^)(void))iptalEt;
+
++ (void)SaniyeliUyariBaslik:(NSString *)baslik
+                     mesaj:(NSString *)mesaj
+                    saniye:(NSTimeInterval)saniye;
+
+@end
+
+NS_ASSUME_NONNULL_END
